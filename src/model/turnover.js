d8bb77e971e8a9dbfb5dcc30ecc118f490dd5888
MarcinRG <marcin.guszpit@gmail.com>
2023-04-12 14:56:11 +0200

    reszta

--- src/model/turnover.js
+++ src/model/turnover.js
@@ -1,1 +1,11 @@
 const { getElement, getAllElements, changeElement, addElement } = require("./commonDbModel");
+
+const ElementQuery = "SELECT * FROM `TURNOVER` WHERE ID=?";
+const AllElementsQuery = "SELECT * FROM `TURNOVER`";
+const AddElementQuery =
+  "INSERT INTO `TURNOVER`(`date`, `netto`, `vat`, `id_tax_rate`, `id_client`, `doc_number`, `id_operation`, `id_sale_branch`) VALUES (?,?,?,?,?,?,?)";
+const UpdateElementQuery =
+  "UPDATE `TURNOVER` SET `date`=?,`netto`=?,`vat`=?,`id_tax_rate`=?,`id_client`=?,`doc_number`=?,`id_operation`=?,`id_sale_branch`=? WHERE ID = ?";
+const RemoveElementQuery = 'DELETE FROM `TURNOVER` WHERE ID=?';
+
+
--- src/routesAndControllers/clientsApi.js
+++ src/routesAndControllers/clientsApi.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { getAllClients, addClient, changeClient, removeClient, getClient} = require('./../model/clients');
+const { getAllClients, addClient, changeClient, removeClient, getClient } = require('./../model/clients');
 const {
     renderAllElements,
     renderAddNewElement,
     renderUpdateElement,
     renderOneElement,
     renderRemoveElement
-  } = require("./commonApi");
+} = require("./commonApi");
+const { check } = require("express-validator");
 const router = express.Router();
 
 router.get('/clients/all', renderAll);
@@ -20,19 +21,19 @@ router.post('/clients/add-new', check("short_name")
 router.patch('/clients/update', [
     check("id").isInt().withMessage("Musisz podać nr id elementu!"),
     check("short_name")
-    .notEmpty()
-    .withMessage("Skrócona nazwa nie może być pusta")
-    .isLength({ max: 35 })
-    .withMessage("Nazwa nie może być dłuższa niż 35 znaków!")
+        .notEmpty()
+        .withMessage("Skrócona nazwa nie może być pusta")
+        .isLength({ max: 35 })
+        .withMessage("Nazwa nie może być dłuższa niż 35 znaków!")
 ],
-renderUpdate);
+    renderUpdate);
 
 router.delete('/clients/delete/:id', renderRemove);
 
 router.get('/clients/elem/:id', renderOne);
 
 function renderAll(req, res, next) {
-    renderAllElements(req, res, next,getAllClients);
+    renderAllElements(req, res, next, getAllClients);
 }
 
 function renderAddNew(req, res, next) {
@@ -51,7 +52,7 @@ function renderRemove(req, res, next) {
 
 function renderOne(req, res, next) {
     renderOneElement(req, res, next, getClient);
-  }
+}
 
 
 module.exports = router;
